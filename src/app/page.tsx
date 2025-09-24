df81cbb3e465d6e894724a4eb8d8e31c69cc64bb
devsujalpatel <sujal7455@gmail.com>
2025-09-24 17:42:27 +0530

    learned about themes in tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center bg-neutral-900">
+    <div className="flex h-screen justify-center bg-primary">
       <Hero />
     </div>
   );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <div className="my-44 flex w-full flex-col items-center selection:bg-[#67E8F9]">
+    <div className="my-44 flex w-full flex-col items-center selection:bg-secondary">
       <h1 className="max-w-3xl bg-gradient-to-b from-neutral-50 to-neutral-500 bg-clip-text text-center text-7xl leading-20 font-bold tracking-tight text-transparent selection:text-neutral-700">
         Unleash the power of intuitive finance
       </h1>
@@ -18,9 +18,9 @@ export const Hero = () => {
           <div className="absolute inset-x-0 bottom-0 h-px w-full bg-gradient-to-r from-transparent via-[#4cb2bf] to-transparent transition-all duration-200 group-hover:top-0"></div>
           Join the waitlist
         </button>
-        <button className="flex items-center flex-col mt-8 text-neutral-400 text-sm">
+        <button className="flex items-center flex-col mt-8 text-neutral-400 group hover:text-neutral-100 text-sm cursor-pointer">
           Learn more
-          <ArrowDown className="text-sm mt-2" width={18} height={18} />
+          <ArrowDown className="text-sm mt-1 group-hover:mt-3 duration-200 transition-all" width={18} height={18} />
         </button>
       </div>
     </div>
