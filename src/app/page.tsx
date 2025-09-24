01083592bbca22c22cbc35071177b798b3a0cb3d
devsujalpatel <sujal7455@gmail.com>
2025-09-24 19:37:35 +0530

    learned about responsive breakpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center bg-primary">
-      <Hero />
+    <div className="h-screen w-full flex items-center justify-center">
+     
     </div>
   );
 }
---
+++ src/components/responsive.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Responsive = () => {
+  return <div className="text-4xl text-black lg:bg-blue-500 sm:text-6xl ">Responsive</div>;
+};
